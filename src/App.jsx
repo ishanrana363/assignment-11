8f18b7b0212c0ea05a7cb391b34ea28bbc8e9ded
mernishan <ishanrana500510@gmail.com>
2023-10-24 18:25:58 +0600

    basick file folder structure done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import About from './components/About';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Project from './components/Project';
+import Service from './components/Service';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Service/>
+      <Project/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
